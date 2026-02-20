ce8ac9bacbac8044206e3294a8a931f1d145ca97
Obada Khalili <obada.akhalili@gmail.com>
2026-02-20 05:48:09 +0200

    dynamic page stale time

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // for dynamic pages, by default stale time is 0 (not cached), that means if you visit a dynamic route and the component takes time to resolve, then you go back to it after visiting another component, it will re-render again
+  // experimental: {
+  //   staleTimes: {
+  //     dynamic: 30
+  //   },
+  // },
 };
 
 export default nextConfig;
